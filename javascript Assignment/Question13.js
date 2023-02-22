25bfe8ae49cf7fa8e1fba402a1f1000ab02d8b24
Sharad Yuvraj Patil <sharad.patil@Yash.com>
2023-02-22 16:36:24 +0530

    final

--- javascript Assignment/Question13.js
+++ javascript Assignment/Question13.js
@@ -3,5 +3,5 @@ function without_first_end(str) {
     return str.substring(1, str.length - 1);
   }
   console.log(without_first_end('JavaScript'));
-  console.log(without_first_end('JS'));
-  console.log(without_first_end('STS'));
+  console.log(without_first_end('JaaaaaS'));
+  console.log(without_first_end('SaaaTS'));
